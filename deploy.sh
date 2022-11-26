9a9b86e4abc72aff4118362295153bcc1decc82f
fosseddy <markfosseddy@gmail.com>
2022-11-26 20:40:43 +0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bach
+
+set -e
+
+npm run build
+
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:fosseddy/phypro.git master:gh-pages
+
+cd -
