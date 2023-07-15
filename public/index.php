8acfb25df1d9ee362817a51b3eac29014b905c8c
fosseddy <markfosseddy@gmail.com>
2023-07-15 05:47:34 +0500

    patch request validation, cleanup, formatting

--- public/index.php
+++ public/index.php
@@ -13,6 +13,8 @@ function debug($val)
 }
 
 require_once LIB_DIR . "/web.php";
+require_once LIB_DIR . "/view.php";
+require_once LIB_DIR . "/path.php";
 require_once LIB_DIR . "/database.php";
 
 $app = new web\App();
@@ -21,157 +23,232 @@ $app->ctx = [
     "db" => new database\Connection("localhost", "phypro", "user", "1234")
 ];
 
-$app->add_route("GET", "/", function() { web\render_view("index"); });
+$app->router->get("/", function() {
+    view\render("index");
+});
 
-$app->add_route(
-    "POST",
-    "/api/month",
-    function(array $ctx) {
-        $body = json_decode(file_get_contents("php://input"), true);
+$app->router->post("/api/month", function(array $ctx) {
+    [
+        "db" => $db,
+        "body" => $body
+    ] = $ctx;
 
-        $name = trim($body["name"] ?? "");
+    $name = trim($body["name"] ?? "");
 
-        if (!$name) throw new http\Bad_Request("name is required");
+    if (!$name) throw new http\Bad_Request("name is required");
 
-        $res["data"]["name"] = $name;
+    $res["name"] = $name;
 
-        $ctx["db"]->transaction();
-        try
+    $db->transaction();
+    try
+    {
+        $db->exec("insert into month (name) values (?)", [$name]);
+        $month_id = (int) $db->last_id();
+
+        $res["id"] = $month_id;
+
+        $sql = "insert into day (month_id, value) values ";
+        $binds = [];
+
+        for ($day = 1; $day <= 31; $day++)
         {
-            $ctx["db"]->exec("insert into month (name) values (?)", [$name]);
-            $month_id = (int) $ctx["db"]->last_id();
-
-            $res["data"]["id"] = $month_id;
-
-            $sql = "insert into day (month_id, value) values ";
-            $binds = [];
-
-            for ($day = 1; $day <= 31; $day++)
-            {
-                if ($day > 1) $sql .= ", ";
-                $sql .= "(?, ?)";
-                $binds[] = $month_id;
-                $binds[] = $day;
-            }
-
-            $ctx["db"]->exec($sql, $binds);
-            $ctx["db"]->commit();
-        }
-        catch (\Exception $e)
-        {
-            $ctx["db"]->rollback();
-            throw $e;
+            if ($day > 1) $sql .= ", ";
+            $sql .= "(?, ?)";
+            $binds[] = $month_id;
+            $binds[] = $day;
         }
 
-        $res["data"]["days"] = $ctx["db"]->query_many(
-            "select id, weight, workout, supplement, value
-             from day where month_id = ?",
-            [$month_id]
-        );
-
-        http_response_code(201);
-        echo json_encode($res);
+        $db->exec($sql, $binds);
+        $db->commit();
     }
-);
-
-// TODO(art): add pagination
-$app->add_route(
-    "GET",
-    "/api/month",
-    function(array $ctx) {
-        $months = $ctx["db"]->query_many(
-            "select id, name from month order by created_at desc"
-        );
-
-        foreach ($months as $i => $it)
-        {
-            $months[$i]["days"] = $ctx["db"]->query_many(
-                "select id, weight, workout, supplement, value from day
-                 where month_id = ? order by value",
-                [$it["id"]]
-            );
-        }
-
-        http_response_code(200);
-        echo json_encode(["data" => ["items" => $months]]);
+    catch (\Exception $e)
+    {
+        $db->rollback();
+        throw $e;
     }
-);
 
-$app->add_route(
-    "DELETE",
-    "/api/month",
-    function(array $ctx) {
-        $id = $ctx["id"];
+    $res["days"] = $db->query_many(
+        "select id, weight, workout, supplement, value
+         from day where month_id = ?",
+        [$month_id]
+    );
 
-        $month = $ctx["db"]->query_one(
-            "select name from month where id = ?",
-            [$id]
+    http_response_code(201);
+    echo json_encode(["data" => $res]);
+}, ["with_json_body"]);
+
+$app->router->get("/api/month", function(array $ctx) {
+    $db = $ctx["db"];
+
+    $per_page = 5;
+    $page_index = 1;
+    ["count" => $items_total] = $db->query_one(
+        "select count(*) as count from month"
+    );
+
+    if (isset($_GET["page"]))
+    {
+        $v = filter_var($_GET["page"], FILTER_VALIDATE_INT, [
+            "options" => ["min_range" => 1]
+        ]);
+        if ($v !== false) $page_index = $v;
+    }
+
+    $stmt = $db->pdo->prepare(
+        "select id, name from month
+         order by created_at desc
+         limit :limit offset :offset"
+    );
+
+    $stmt->bindValue(
+        "offset",
+        ($page_index - 1) * $per_page,
+        \PDO::PARAM_INT
+    );
+    $stmt->bindParam("limit", $per_page, \PDO::PARAM_INT);
+    $stmt->execute();
+
+    $months = $stmt->fetchAll();
+
+    foreach ($months as $i => $it)
+    {
+        $months[$i]["days"] = $db->query_many(
+            "select id, weight, workout, supplement, value from day
+             where month_id = ? order by value",
+            [$it["id"]]
+        );
+    }
+
+    http_response_code(200);
+    echo json_encode(["data" => [
+        "items" => $months,
+        "items_count" => count($months),
+        "items_per_page" => $per_page,
+        "items_total" => $items_total,
+        "page_index" => $page_index,
+        "page_total" => ceil($items_total / $per_page)
+    ]]);
+});
+
+$app->router->delete("/api/month", function(array $ctx) {
+    [
+        "db" => $db,
+        "id" => $id
+    ] = $ctx;
+
+    $month = $db->query_one("select name from month where id = ?", [$id]);
+
+    if (!$month) throw new http\Not_Found("month not found");
+
+    $db->exec("delete from month where id = ?", [$id]);
+
+    $month["id"] = $id;
+
+    http_response_code(200);
+    echo json_encode(["data" => $month]);
+}, ["require_id"]);
+
+$app->router->patch("/api/day", function(array $ctx) {
+    [
+        "db" => $db,
+        "id" => $id,
+        "body" => $body
+    ] = $ctx;
+
+    $fields = [];
+    $errors = [];
+    $filter_opts = ["options" => ["min_range" => 0, "max_range" => 1]];
+
+    if (isset($body["workout"]))
+    {
+        $fields["workout"] = filter_var(
+            $body["workout"],
+            FILTER_VALIDATE_INT,
+            $filter_opts
         );
 
-        if (!$month) throw new http\Not_Found("month not found");
-
-        $ctx["db"]->exec("delete from month where id = ?", [$id]);
-
-        $month["id"] = $id;
-
-        http_response_code(200);
-        echo json_encode(["data" => $month]);
-    },
-    ["require_id"]
-);
-
-// TODO(art): validate fields
-$app->add_route(
-    "PATCH",
-    "/api/day",
-    function(array $ctx) {
-        $id = $ctx["id"];
-
-        $body = json_decode(file_get_contents("php://input"), true);
-
-        $fields = [];
-        $supported = ["weight", "workout", "supplement"];
-
-        foreach ($array_keys($body) as $key)
+        if ($fields["workout"] === false)
         {
-            if (in_array($key, $supported)) $fields[$key] = $body[$key];
+            unset($fields["workout"]);
+            $errors[] = ["message" => "invalid workout value"];
         }
+    }
 
-        if (!$fields) throw new http\Bad_Request("provide fields to update");
-
-        $day = $ctx["db"]->query_one(
-            "select id, weight, workout, supplement, value
-             from day where id = ?",
-            [$id]
+    if (isset($body["supplement"]))
+    {
+        $fields["supplement"] = filter_var(
+            $body["supplement"],
+            FILTER_VALIDATE_INT,
+            $filter_opts
         );
 
-        if (!$day) throw new http\Not_Found("day not found");
-
-        $sql = [];
-        $vals = [];
-        foreach ($fields as $k => $v)
+        if ($fields["supplement"] === false)
         {
-            $sql[] = "$k = ?";
-            $vals[] = $v;
-            $day[$k] = $v;
+            unset($fields["supplement"]);
+            $errors[] = ["message" => "invalid supplement value"];
         }
+    }
 
-        $sql = join(", ", $sql);
-        $ctx["db"]->exec("update day set $sql where id = ?", [...$vals, $id]);
+    if (isset($body["weight"]))
+    {
+        $filter_opts["options"]["max_range"] = 99900;
 
-        http_response_code(200);
-        echo json_encode(["data" => $day]);
-    },
-    ["require_id"]
-);
+        $fields["weight"] = filter_var(
+            $body["weight"],
+            FILTER_VALIDATE_INT,
+            $filter_opts
+        );
+
+        if ($fields["weight"] === false)
+        {
+            unset($fields["weight"]);
+            $errors[] = ["message" => "invalid weight value"];
+        }
+    }
+
+    if ($errors)
+    {
+        throw new http\Bad_Request("invalid data", ["errors" => $errors]);
+    }
+    else if (!$fields)
+    {
+        throw new http\Bad_Request("provide fields to update");
+    }
+
+    $day = $db->query_one(
+        "select id, weight, workout, supplement, value
+         from day where id = ?",
+        [$id]
+    );
+
+    if (!$day) throw new http\Not_Found("day not found");
+
+    $sql = [];
+    $vals = [];
+    foreach ($fields as $k => $v)
+    {
+        $sql[] = "$k = ?";
+        $vals[] = $v;
+        $day[$k] = $v;
+    }
+
+    $sql = join(", ", $sql);
+    $db->exec("update day set $sql where id = ?", [...$vals, $id]);
+
+    http_response_code(200);
+    echo json_encode(["data" => $day]);
+}, ["require_id", "with_json_body"]);
 
 function require_id(array &$ctx)
 {
     $id = trim($_GET["id"] ?? "");
-
     if (!$id) throw new http\Bad_Request("id is required");
-
     $ctx["id"] = $id;
+}
+
+function with_json_body(array &$ctx)
+{
+    $ctx["body"] = json_decode(file_get_contents("php://input"), true) ?? [];
 }
 
 try
@@ -181,30 +258,12 @@ try
 catch (http\Error $e)
 {
     http_response_code($e->status_code);
-
-    $type = $_SERVER["HTTP_CONTENT_TYPE"] ?? "";
-
-    if ($type === "application/json")
-    {
-        echo json_encode(["error" => $e->getData()]);
-        exit;
-    }
-
-    echo $e->getMessage();
+    echo json_encode(["error" => $e->getData()]);
 }
-catch (\Exception $e)
+catch (\Throwable $e)
 {
     error_log($e->getMessage());
 
     http_response_code(500);
-
-    $type = $_SERVER["HTTP_CONTENT_TYPE"] ?? "";
-
-    if ($type === "application/json")
-    {
-        echo json_encode(["error" => ["message" => "internal server error"]]);
-        exit;
-    }
-
-    echo "Something went wrong and it is not your fault";
+    echo json_encode(["error" => ["message" => "internal server error"]]);
 }
